c8173ec5f3d1fd4c50479ad5f5fe838128eb28ed
kirzyka <kirzyka@gmail.com>
2024-08-09 02:18:51 +0300

    version

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let msg = document.getElementById('msg');
 let canvas = document.createElement('canvas');
 let canvasContext = canvas.getContext('2d');
 let currentStream = null; // Хранит текущий видеопоток
-let scanAttempts = 0; // Счётчик попыток сканирования
 
 function setMsg(message) {
     msg.innerHTML = message;
@@ -36,8 +35,6 @@ async function initCamera() {
 
 function scan() {
     if (currentStream) {
-        scanAttempts++; // Увеличиваем счётчик попыток
-
         // Установим размеры canvas равными размерам video
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
@@ -60,7 +57,7 @@ function scan() {
             if (code) {
                 setMsg("QR-код найден: " + code.data);
             } else {
-                setMsg(`Ищем QR-код... Попытка #${scanAttempts}`);
+                setMsg("Ищем QR-код...");
             }
         } catch (error) {
             setMsg('Ошибка сканирования: ' + error.message);
