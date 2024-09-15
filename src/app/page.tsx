6bd7baa57a4de285490fc27245b2bced934006ff
Anton Kirzyk <kirzyka@gmail.com>
2024-09-15 22:55:45 +0300

    simple

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
-import dynamic from 'next/dynamic';
+//import dynamic from 'next/dynamic';
 
-const Scaner = dynamic(() => import('../component/Scaner'), { ssr: false });
+//const Scaner = dynamic(() => import('../component/Scaner'), { ssr: false });
 
 
 export default function Home() {
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Scaner/>
+        <h1>FreeCup</h1>
       </main>
     </div>
   );
