cd5309e151d30522c8855d22a825992b8137d495
Anton Kirzyk <kirzyka@gmail.com>
2024-09-24 09:30:05 +0300

    bot

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -7,6 +7,32 @@ import { Bot, webhookCallback } from 'grammy';
 const bot = new Bot(BOT_KEY);
 
 bot.on('message:text', async (ctx) => {
+  const chatId = ctx.chat.id;
+  const text = ctx.message.text;
+
+  if (text === '/start') {
+    await ctx.reply('Hello, I am a bot. I can do anything you want me to. Just send me a message and I\'ll reply you!');
+    return;
+  }
+
+  if (text === '/stop') {
+    await ctx.reply('Bye!');
+    bot.stop();
+    return;
+  }
+
+  if (text === '/chat') {
+    await ctx.reply(`Your chat id: ${chatId}`);
+    return;
+  }
+
+  if (text === '/link') {
+    await bot.api.sendMessage(chatId, 'https://free-cup-tg.vercel.app/', {
+      parse_mode: 'Markdown',
+    });
+    return;
+  }
+
   await ctx.reply(ctx.message.text);
 })
 
--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,5 @@
 export const BOT_KEY: string = "6871271593:AAE3P4iB4-qUsUYzI0MBzRRYl80pyDvVUV8";
+// https://api.telegram.org/bot6871271593:AAE3P4iB4-qUsUYzI0MBzRRYl80pyDvVUV8/setWebhook?url=https://free-cup-tg.vercel.app/api/bot
 /*
 background: var(--tg-theme-bg-color) - Цвет заднего фона под тему Telegram
 
