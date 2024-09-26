40a34308153d17314e43668e513b4f75b97f7730
kirzyka <kirzyka@gmail.com>
2024-09-26 22:04:17 +0300

    back on scan

--- src/component/view/scan/ScanView.tsx
+++ src/component/view/scan/ScanView.tsx
@@ -1,70 +1,48 @@
 "use client";
 
-//import { TelegramWebAppContainer,  } from "@telegram-web-app/core";
+import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
 import Button from "@/component/button/Button";
 
+
 const ScanView = () => {
-  const [webApp, setWebApp] = useState<WebApp | null>(null); 
+  const router = useRouter();
   const [msg, setMsg] = useState("");
   const [code, setCode] = useState("");
   const [error, setError] = useState("");
 
-  const openScanQrPopup = () => {  
-    try {
-      webApp!.showScanQrPopup({
-        text: 'Пожалуйста, отсканируйте QR-код',
-        
-      }, (result: string | null) => { 
-        if (result) {
-          setCode(result);
-          setMsg(`QR-код найден: ${result}`);
-        } else {
-          setMsg('QR-код не найден');
-        }
-        //webApp.closeScanQrPopup();
-        return true;
-      });        
-    } catch (e: unknown) {
-      setError((e as Error).message);
-    }    
-  };
-
   useEffect(() => {
     if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-      const _webApp = window.Telegram.WebApp;
+      const webApp = window.Telegram.WebApp;
 
-      _webApp.ready();
-      _webApp.onEvent('scanQrPopupClosed', () => {
+      webApp.ready();
+      webApp.onEvent('scanQrPopupClosed', () => {
         setMsg('Окно закрыто');
+        router.back();
       }); 
 
-      setWebApp(_webApp);
-      openScanQrPopup();
+      try {
+        webApp!.showScanQrPopup({
+          text: 'Пожалуйста, отсканируйте QR-код',
+          
+        }, (result: string | null) => { 
+          if (result) {
+            setCode(result);
+            setMsg(`QR-код найден: ${result}`);
+          } else {
+            setMsg('QR-код не найден');
+          }
+          return true;
+        });        
+      } catch (e: unknown) {
+        setError((e as Error).message);
+      }    
       //console.log("Пользователь Telegram:", webApp.initDataUnsafe?.user?.first_name);
       //webApp.MainButton.setText("Нажмите меня!");
       //webApp.MainButton.show();
     }
   }, []);
 
-  useEffect(() => {
-    if (webApp) {
-      openScanQrPopup();
-    } else {
-      setMsg('Telegram WebApp API не доступен.');
-    }  
-  }, [webApp]);
- /* 
-  useEffect(() => {
-    webApp.onEvent('popupClosed', () => { //scanQrPopupClosed
-      setMsg('Окно закрыто');
-    }); 
-    webApp.onEvent('qrTextReceived', (data: QrTextReceivedCallbackData) => {
-      setMsg('Код получен:' + data.data);
-    });
-    openScanQrPopup();
-  }, []); 
-*/
   return (
     <div className="flex items-center w-full h-full justify-items-center [family-name:var(--font-geist-sans)]">
       <main className="flex flex-col w-full h-full gap-8 items-center justify-between">
@@ -75,7 +53,6 @@ const ScanView = () => {
           <p>{error}</p>
       </div>
       <footer className='flex flex-col gap-1 w-full p-3'>
-        <Button label="Scan again" onClick={openScanQrPopup}/>
         <Button label="Back" url="/"/>
       </footer>        
       </main>
