c4ec9f5d35c2d214afec795feb662c8dc4f5155c
kirzyka <kirzyka@gmail.com>
2024-09-26 21:42:46 +0300

    scan refactoring

--- src/component/view/scan/ScanView.tsx
+++ src/component/view/scan/ScanView.tsx
@@ -5,57 +5,45 @@ import { useEffect, useState } from "react";
 import Button from "@/component/button/Button";
 
 const ScanView = () => {
-  //const telegram = new TelegramWebAppContainer();
-  //const webApp = telegram.WebApp;
+  const [webApp, setWebApp] = useState<WebApp | null>(null); 
   const [msg, setMsg] = useState("");
   const [code, setCode] = useState("");
-  //const [error, setError] = useState("");
+  const [error, setError] = useState("");
 
-/*
   const openScanQrPopup = () => {  
     try {
-      webApp.showScanQrPopup({
-        text: 'Пожалуйста, отсканируйте QR-код',
-        
-      }, (result: string | null) => { 
-        if (result) {
-          setCode(result);
-          setMsg(`QR-код найден: ${result}`);
-        } else {
-          setMsg('QR-код не найден');
-        }
-        //webApp.closeScanQrPopup();
-        return true;
-      });
+      if(webApp) {
+        webApp.showScanQrPopup({
+          text: 'Пожалуйста, отсканируйте QR-код',
+          
+        }, (result: string | null) => { 
+          if (result) {
+            setCode(result);
+            setMsg(`QR-код найден: ${result}`);
+          } else {
+            setMsg('QR-код не найден');
+          }
+          //webApp.closeScanQrPopup();
+          return true;
+        });
+      }      
     } catch (e: unknown) {
       setError((e as Error).message);
-    }
-    
+    }    
   };
-*/
+
   useEffect(() => {
     if (typeof window !== 'undefined' && window.Telegram?.WebApp) {
-      const webApp = window.Telegram.WebApp;
-      webApp.ready(); // Сообщаем, что приложение готово
+      const _webApp = window.Telegram.WebApp;
 
-      webApp.onEvent('popupClosed', () => { //scanQrPopupClosed
+      _webApp.ready();
+      _webApp.onEvent('scanQrPopupClosed', () => {
         setMsg('Окно закрыто');
       }); 
-      
-      webApp.showScanQrPopup({
-        text: 'Пожалуйста, отсканируйте QR-код',
-        
-      }, (result: string | null) => { 
-        if (result) {
-          setCode(result);
-          setMsg(`QR-код найден: ${result}`);
-        } else {
-          setMsg('QR-код не найден');
-        }
-        //webApp.closeScanQrPopup();
-        return true;
-      });
-      console.log("Пользователь Telegram:", webApp.initDataUnsafe?.user?.first_name);
+
+      setWebApp(_webApp);
+      openScanQrPopup();
+      //console.log("Пользователь Telegram:", webApp.initDataUnsafe?.user?.first_name);
       //webApp.MainButton.setText("Нажмите меня!");
       //webApp.MainButton.show();
     }
@@ -78,6 +66,7 @@ const ScanView = () => {
           <h1 className='text-3xl'>Scan result:</h1>
           <p>{code}</p>
           <p>{msg}</p>
+          <p>{error}</p>
       </div>
       <footer className='flex flex-col gap-1 w-full p-3'>
         <Button label="Back" url="/"/>
