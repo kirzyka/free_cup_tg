0ff0cad64c7c5b7adf38db14f04da96181e04c7c
Anton Kirzyk <kirzyka@gmail.com>
2024-09-27 23:58:51 +0300

    lang

--- src/hooks/useSettings.ts
+++ src/hooks/useSettings.ts
@@ -3,14 +3,14 @@ import { TRANSLATIONS } from '@/i18n/Translations';
 import Settings from '@/types/Settings';
 
 export function useSettings(): Settings {
-  let deviceLanguage: string = DEFAULT_LANG;
+  let appLang: string = DEFAULT_LANG;
 
   if (typeof window !== "undefined") {
     const langs: string[] = Object.keys(TRANSLATIONS);
     const clientLang: string = navigator.language || DEFAULT_LANG;
 
-    deviceLanguage = langs.find(l => clientLang.includes(l)) || DEFAULT_LANG;
+    appLang = langs.find((lang: string) => clientLang.includes(lang)) || DEFAULT_LANG;
   }
 
-  return { language: deviceLanguage };
+  return { language: appLang };
 }
