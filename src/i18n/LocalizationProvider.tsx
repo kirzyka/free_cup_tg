f79e4eecdd204df7348429a8e05b8f6ceacadc75
kirzyka <kirzyka@gmail.com>
2024-09-29 18:00:42 +0300

    locale routing

--- src/i18n/LocalizationProvider.tsx
+++ src/i18n/LocalizationProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useContext } from 'react';
+import { createContext } from 'react';
 import { TRANSLATIONS } from '@/i18n/Translations';
 import { DEFAULT_LANG } from '@/client_const';
 
