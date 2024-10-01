a4e2b396e499b3326b85a613e2bdd5eb5cdc4820
Anton Kirzyk <kirzyka@gmail.com>
2024-10-01 14:59:41 +0300

    fix build

--- src/app/[lang]/points/page.tsx
+++ src/app/[lang]/points/page.tsx
@@ -1,11 +1,10 @@
-import Button from "@/component/button/Button";
+
 
 export default function Points() {
     return (
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <h1>Coffee Shops</h1>
-          <Button label="Main" url="/"/>
         </main>
       </div>
     );
