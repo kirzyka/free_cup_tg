68fb7ee2bbef5b8c9eef9b4c28b652953cff1bdb
kirzyka <kirzyka@gmail.com>
2024-10-03 01:21:02 +0300

    points

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import MainView from '@/component/view/main/MainView';
+import { AppContext } from '@/context/AppContextProvider';
+import { useContext } from 'react';
 
 export default function Page() {
-  return (
-    <MainView/>
-  );
+  const {points} = useContext(AppContext);
+
+  return !points || !points.length ? <MainView/> : <div><p>POINTS</p>{points.map(p => <p key={p.key}>{p.name}</p>)}</div>;
 }
