6a1121e04f9e7a67c2483a00687c874574aee7dd
kirzyka <kirzyka@gmail.com>
2024-10-05 00:20:19 +0300

    crypto

--- src/utils/codeUtils.ts
+++ src/utils/codeUtils.ts
@@ -18,25 +18,25 @@ export const generateAddPointQRCode = async (point: Point) => {
 export const generateClonePointQRCode = async (point: Point) => {
     const timestamp = Date.now();
     const data = {
-      a: Action.CLONE_POINT,
-      i: point.key,
-      n: point.name,
-      c: point.requiredCups,
-      k: point.accessKey,
-      t: timestamp,
+        a: Action.CLONE_POINT,
+        i: point.key,
+        n: point.name,
+        c: point.requiredCups,
+        k: point.accessKey,
+        t: timestamp,
     };
 
     return encryptData(JSON.stringify(data), DATA_KEY);
-  };
+};
 
-  export const generateAddCupQRCode = async (point: Point) => {
+export const generateAddCupQRCode = async (point: Point) => {
     const timestamp = Date.now();
     const data = {
-      a: Action.ADD_CUP,
-      i: point.key,
-      t: timestamp,
+        a: Action.ADD_CUP,
+        i: point.key,
+        t: timestamp,
     };
     const signature: string = await generateHMAC(data, point.accessKey);
 
     return encryptData(JSON.stringify({ ...data, s: signature }), DATA_KEY);
-  };
+};
--- src/utils/cryptoUtils.ts
+++ src/utils/cryptoUtils.ts
@@ -1,46 +1,53 @@
 //import * as crypto from 'crypto';
-import CryptoJS from 'crypto-js';
+import CryptoJS from "crypto-js";
 
 export const encryptData = (text: string, key: string): string => {
-  // Генерация случайного вектора инициализации (IV)
-  const ivArray = crypto.getRandomValues(new Uint8Array(16));//crypto.randomBytes(16);
-  const iv = CryptoJS.lib.WordArray.create(ivArray);
+    // Генерация случайного вектора инициализации (IV)
+    const ivArray = crypto.getRandomValues(new Uint8Array(16)); //crypto.randomBytes(16);
+    const iv = CryptoJS.lib.WordArray.create(ivArray);
 
-  // Хэширование ключа для использования в AES
-  const hashedKey = CryptoJS.SHA256(key).toString();
+    // Хэширование ключа для использования в AES
+    const hashedKey = CryptoJS.SHA256(key).toString();
 
-  // Шифрование текста с использованием ключа и вектора инициализации (IV)
-  const encrypted = CryptoJS.AES.encrypt(text, CryptoJS.enc.Hex.parse(hashedKey), {
-    iv: iv
-  }).toString();
+    // Шифрование текста с использованием ключа и вектора инициализации (IV)
+    const encrypted = CryptoJS.AES.encrypt(
+        text,
+        CryptoJS.enc.Hex.parse(hashedKey),
+        {
+            iv: iv,
+        }
+    ).toString();
 
-  // Конкатенация IV и зашифрованного текста
-  const ivAndEncrypted = iv.toString() + encrypted;
+    // Конкатенация IV и зашифрованного текста
+    const ivAndEncrypted = iv.toString() + encrypted;
 
-  // Преобразование в Base64 для удобства хранения
-  return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(ivAndEncrypted));
+    // Преобразование в Base64 для удобства хранения
+    return CryptoJS.enc.Base64.stringify(
+        CryptoJS.enc.Utf8.parse(ivAndEncrypted)
+    );
 };
 
-
 export async function generateHMAC(
-  data: object,
-  accessKey: string
+    data: object,
+    accessKey: string
 ): Promise<string> {
-  const message = JSON.stringify(data);
-  const enc = new TextEncoder();
-  const keyData = enc.encode(accessKey);
-  const messageData = enc.encode(message);
+    const message = JSON.stringify(data);
+    const enc = new TextEncoder();
+    const keyData = enc.encode(accessKey);
+    const messageData = enc.encode(message);
 
-  const key = await crypto.subtle.importKey(
-    'raw',
-    keyData,
-    { name: 'HMAC', hash: { name: 'SHA-256' } },
-    false,
-    ['sign']
-  );
+    const key = await crypto.subtle.importKey(
+        "raw",
+        keyData,
+        { name: "HMAC", hash: { name: "SHA-256" } },
+        false,
+        ["sign"]
+    );
 
-  const signature = await crypto.subtle.sign('HMAC', key, messageData);
-  const hashArray = Array.from(new Uint8Array(signature));
-  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
-  return hashHex;
+    const signature = await crypto.subtle.sign("HMAC", key, messageData);
+    const hashArray = Array.from(new Uint8Array(signature));
+    const hashHex = hashArray
+        .map((b) => b.toString(16).padStart(2, "0"))
+        .join("");
+    return hashHex;
 }
