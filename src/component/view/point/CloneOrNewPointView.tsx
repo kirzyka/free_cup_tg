36a623de84eeda5f00320d3504ea036192bdc472
Anton Kirzyk <kirzyka@gmail.com>
2024-10-07 22:23:08 +0300

    fix button usage

--- src/component/view/point/CloneOrNewPointView.tsx
+++ src/component/view/point/CloneOrNewPointView.tsx
@@ -2,11 +2,10 @@
 
 import Button from "@/component/button/Button";
 import { useLocale } from "@/hooks/useLocale";
-import { getURL } from "@/utils/routerUtils";
 import { useRouter } from "next/navigation";
 
 const CloneOrNewPointView = () => {
-    const {t, language} = useLocale();
+    const {t} = useLocale();
     const router = useRouter();
 
     return (
@@ -17,7 +16,7 @@ const CloneOrNewPointView = () => {
             </div>
             <footer className='flex flex-col gap-1 w-full p-3'>
                 <Button label={t("SCR_CLONE_OR_NEW_LBL_NEW")} url="/point/register"/>
-                <Button label={t("SCR_CLONE_OR_NEW_LBL_GET")} url={getURL("/scan", language)}/>
+                <Button label={t("SCR_CLONE_OR_NEW_LBL_GET")} url="/scan"/>
                 <Button label={t('CMN_BACK')} onClick={() => router.back()}/>
             </footer>        
             </main>
--- src/component/view/role/RoleView.tsx
+++ src/component/view/role/RoleView.tsx
@@ -2,11 +2,10 @@
 
 import Button from "@/component/button/Button";
 import { useLocale } from "@/hooks/useLocale";
-import { getURL } from "@/utils/routerUtils";
 import { useRouter } from "next/navigation";
 
 const RoleView = () => {
-    const {t, language} = useLocale();
+    const {t} = useLocale();
     const router = useRouter();
 
     return (
@@ -18,7 +17,7 @@ const RoleView = () => {
                 </h1>
             </div>
             <footer className='flex flex-col gap-1 w-full p-3'>
-                <Button label={t("SCR_ROLE_LBL_CLIENT")} url={getURL("/scan", language)}/>
+                <Button label={t("SCR_ROLE_LBL_CLIENT")} url="/scan"/>
                 <Button label={t('SCR_ROLE_LBL_BARISTA')} url="/point/clone-or-new"/>
                 <Button label={t('CMN_BACK')} onClick={() => router.back()}/>
             </footer>        
