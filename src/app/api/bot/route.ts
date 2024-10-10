d2e2fc76dac80f3575381d96db4a50bc70294f5d
Anton Kirzyk <kirzyka@gmail.com>
2024-10-10 21:55:51 +0300

    bot api

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -4,60 +4,43 @@ import { Bot, InlineKeyboard, webhookCallback } from "grammy";
 const bot = new Bot(BOT_KEY);
 
 bot.on("message:text", async (ctx) => {
-    //const chatId = ctx.chat.id;
-    const text = ctx.message.text;
-    const lang = ctx.from.language_code;
-    const imgUrl: string = "https://free-cup-tg.vercel.app/images/cup_c_e.png";
-    const description: string = `${lang} Hello, I am a bot. I can do anything you want me to. Just send me a message and I'll reply you!`;
-    const btnText: string = "Run FreeCup";
+  //const chatId = ctx.chat.id;
+  const text = ctx.message.text;
+  const lang = ctx.from.language_code;
+  const imgUrl: string = "https://free-cup-tg.vercel.app/images/cup_c_e.png";
+  const description: string = `${lang} Hello, I am a bot. I can do anything you want me to. Just send me a message and I'll reply you!`;
+  const btnText: string = "FreeCup";
 
-    if (text === "/start") {
-        await ctx.replyWithPhoto(imgUrl, {
-            caption: description,
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {
-                            text: btnText,
-                            callback_data: "open_mini_app",
-                        },
-                    ],
-                ],
+  if (text === "/start") {
+    await ctx.replyWithPhoto(imgUrl, {
+      caption: description,
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: btnText,
+              web_app: {
+                url: "https://free-cup-tg.vercel.app",
+              },
             },
-        });
-        return;
-    }
+          ],
+        ],
+      },
+    });
+    return;
+  }
 
-    if (text === "/link") {
-        await ctx.reply("FreeCup: ", {
-            reply_markup: new InlineKeyboard().url(
-                "Android version",
-                "https://free-cup-tg.vercel.app"
-            ),
-        });
-        return;
-    }
+  if (text === "/link") {
+    await ctx.reply("FreeCup: ", {
+      reply_markup: new InlineKeyboard().url(
+        "Android version",
+        "https://free-cup-tg.vercel.app"
+      ),
+    });
+    return;
+  }
 
-    await ctx.reply(ctx.message.text);
-});
-
-bot.on("callback_query", async (ctx) => {
-    if (ctx.callbackQuery.data === "open_mini_app") {
-        await ctx.editMessageReplyMarkup({
-            reply_markup: {
-                inline_keyboard: [
-                    [
-                        {
-                            text: "FreeCup",
-                            web_app: {
-                                url: "https://free-cup-tg.vercel.app",
-                            },
-                        },
-                    ],
-                ],
-            },
-        });
-    }
+  await ctx.reply(ctx.message.text);
 });
 
 export const POST = webhookCallback(bot, "std/http");
