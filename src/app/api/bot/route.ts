793ebb63c0cdcd1d38698cd06298efa3c8c9a979
kirzyka <kirzyka@gmail.com>
2024-10-10 17:38:26 +0300

    bot api

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -1,39 +1,31 @@
-//export const dynamic = 'force-dynamic';
-//export const fetchCache = 'force-no-store';
-
-import { BOT_KEY } from '@/constServer';
-import { Bot, InlineKeyboard, webhookCallback } from 'grammy';
+import { BOT_KEY } from "@/constServer";
+import { Bot, InlineKeyboard, webhookCallback } from "grammy";
 
 const bot = new Bot(BOT_KEY);
 
-bot.on('message:text', async (ctx) => {
-  const chatId = ctx.chat.id;
-  const text = ctx.message.text;
+bot.on("message:text", async (ctx) => {
+    //const chatId = ctx.chat.id;
+    const text = ctx.message.text;
+    const lang = ctx.from.language_code;
 
-  if (text === '/start') {
-    await ctx.reply('Hello, I am a bot. I can do anything you want me to. Just send me a message and I\'ll reply you!');
-    return;
-  }
+    if (text === "/start") {
+        await ctx.reply(
+            lang || "lang is undefined" //""Hello, I am a bot. I can do anything you want me to. Just send me a message and I'll reply you!"
+        );
+        return;
+    }
 
-  if (text === '/stop') {
-    await ctx.reply('Bye!');
-    bot.stop();
-    return;
-  }
+    if (text === "/link") {
+        await ctx.reply("FreeCup: ", {
+            reply_markup: new InlineKeyboard().url(
+                "Android version",
+                "https://free-cup-tg.vercel.app"
+            ),
+        });
+        return;
+    }
 
-  if (text === '/chat') {
-    await ctx.reply(`Your chat id: ${chatId}`);
-    return;
-  }
+    await ctx.reply(ctx.message.text);
+});
 
-  if (text === '/link') {
-    await ctx.reply('FreeCup: ', {
-      reply_markup: new InlineKeyboard().url('Link', 'https://free-cup-tg.vercel.app'),
-    });
-    return;
-  }
-
-  await ctx.reply(ctx.message.text);
-})
-
-export const POST = webhookCallback(bot, 'std/http');
+export const POST = webhookCallback(bot, "std/http");
